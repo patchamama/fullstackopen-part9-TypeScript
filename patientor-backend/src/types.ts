17210b00d8451cee548e2fec57ff0d8c375b58b7
A. Urquiola <patchamama.go@gmail.com>
2023-12-06 19:35:48 +0100

    solution 9.11: Patientor backend, step4

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -9,3 +9,23 @@ export interface DiagnoseEntry {
   name: string;
   latin?: string;
 }
+
+// {
+//     id: 'd2773336-f723-11e9-8f0b-362b9e155667',
+//     name: 'John McClane',
+//     dateOfBirth: '1986-07-09',
+//     ssn: '090786-122X',
+//     gender: 'male',
+//     occupation: 'New york city cop',
+//   },
+
+export interface PatientEntry {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
